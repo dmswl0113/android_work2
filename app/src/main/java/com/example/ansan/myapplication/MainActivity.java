dc58f8a11168e5946dcdcb63049d8e561f113f7f
LeeEunJi <a33981027@gmail.com>
2016-11-02 12:26:58 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.ansan.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button: //폴더 생성
+                break;
+            case R.id.button2:
+                break;
+            case R.id.button3:
+                break;
+            case R.id.button4:
+                break;
+            case R.id.button5:
+                break;
+        }
+    }
+}
