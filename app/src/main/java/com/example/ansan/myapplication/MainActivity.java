73a64573596956ebfbc14fe28139eb051af97d26
LeeEunJi <a33981027@gmail.com>
2016-11-02 13:17:03 +0900

    파일 목록 가져오기 기능 추가

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.File;
@@ -18,11 +19,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    EditText e1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        e1 = (EditText)findViewById(R.id.editText);
     }
 
     public void onClick(View v){
@@ -76,6 +79,16 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case R.id.button5: //파일 목록 가져오기
+                File filelist[] = new File(path).listFiles();
+                String str = "";
+                for(int i = 0; i < filelist.length; i++){
+                    if(filelist[i].isDirectory()){
+                        str += "<폴더>" + filelist[i].toString() +"\n";
+                    } else {
+                        str += "<파일>" + filelist[i].toString() +"\n";
+                    }
+                }
+                e1.setText(str);
                 break;
         }
     }
